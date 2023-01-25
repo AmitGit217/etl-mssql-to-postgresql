29c722e3ddd4817da9040d03b7e519807fd6de8f
DESKTOP-0C0H0I1\moshe <citysys.ltd@gmail.com>
2023-01-25 10:46:09 +0200

    chore: simple tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getAllUsers() {
+    return prisma.user.findMany({});
+  }
+
+  async getAllPosts() {
+    return prisma.post.findMany({});
+  }
+
+  async getPostsByUserId(id: string) {
+    return prisma.user.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        posts: true,
+      },
+    });
   }
 }
